f9dde456807dbcf49028d2c90d0cd91080426c05
jworkss <flytoshine@gmail.com>
2022-05-09 22:18:59 -0300

    Agregado los rutes fijar si se puede mejorar codigo

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -7,27 +7,11 @@ import { PortfolioService } from 'src/app/servicios/portfolio.service';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  misTrabajos: any;
-  misFrontEnd: any;
-  misBackEnd: any;
+
 
   constructor(private datosPortfolio: PortfolioService) { }
 
   ngOnInit(): void {
-    this.datosPortfolio.obtenerDatos().subscribe(data => {
-      this.misTrabajos = data.proyectos;
-    })
-
-    this.datosPortfolio.obtenerDatos().subscribe(data => {
-      this.misFrontEnd = data.skills.frontEnd;
-    })
-
-    this.datosPortfolio.obtenerDatos().subscribe(data => {
-      this.misBackEnd = data.skills.backEnd;
-    })
-
-
-
 
   }
 
